5fbdf996d65ff1bd3ddaa5f8b681f5ddf4a344d2
Gustavo Torres <contato.gustavotorres@gmail.com>
2024-03-15 02:20:56 -0300

    start
    criação das rotas e exibição das views necessárias para o projeto.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [homeController::class, 'index']);
+Route::get('/product', [ProductController::class, 'show']);
+
+
+
+
+//Admin
+
+Route::get('/admin/products', [AdminProductController::class, 'index']);
+Route::get('/admin/edit', [AdminProductController::class, 'edit']);
