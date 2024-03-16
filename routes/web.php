aaf95a731cf85b5aba85b7c987cf2b8e76ee4c3f
Gustavo Torres <contato.gustavotorres@gmail.com>
2024-03-15 21:26:15 -0300

    add slug
    Adicionado navegação por slug na rota dos produtos.

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [homeController::class, 'index'])->name('home');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('product');
+Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product');
 
 
 
